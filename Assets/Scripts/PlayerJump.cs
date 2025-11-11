68bcfef5c473e365503278a8c8edd6a3d015e9da
SebasGirald1003 <sebasgiraldo1003@gmail.com>
2025-11-10 19:36:13 -0500

    cambio de gravedad implementado

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -10,12 +10,16 @@ public class PlayerJump : MonoBehaviour
     private bool isGrounded;
     public float groundCheckRadius = 0.2f;
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+         void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
@@ -23,9 +27,25 @@ public class PlayerJump : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-
-        Debug.Log(isGrounded);
     }
+
+ void FixedUpdate()
+    {
+        // --- Better Falling Logic ---
+        if (rb.linearVelocity.y < 0)
+        {
+            // We are falling - apply the fallMultiplier
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            // We are rising, but not holding Jump - apply the lowJumpMultiplier
+            // This replaces the GetButtonUp logic from Step 2 for a smoother feel
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+
 
     private void OnDrawGizmosSelected()
     {
